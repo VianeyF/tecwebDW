7055ec1b85ab9e68bd74b2776e9315102ba57632
fvianey <rocio.floresfl@alumno.buap.mx>
2024-08-30 03:23:43 -0600

    Cambio en de, se agregaron archovos .html .php

---
+++ practicas/p04/inclusion_archivos/pie.inc.php
@@ -0,0 +1,13 @@
+<hr />
+<?php
+echo "<div>
+        <h1 style=\"border-width:3px; border-style:groove; background-color:#ffcc99;\">
+        Final de la página PHP Vínculos útiles: 
+        <a href=\"https://php.net\">php.net</a> &nbsp;
+        <a href=\"https://mysql.org\">mysql.org</a>
+        </h1>";
+echo "Nombre del archivo ejecutado: " . $_SERVER['PHP_SELF'] . "&nbsp;&nbsp;&nbsp;";
+echo "Nombre del archivo incluido: " . __FILE__ . "</div>";
+?>
+</body>
+</html>
