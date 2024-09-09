27712fa868854d554f4b507dc278c4a9303cc77e
fvianey <rocio.floresfl@alumno.buap.mx>
2024-09-09 02:28:43 -0600

    Cambio en dev, se agregaron nuevos valores a variables y uso de GLOBALS

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -51,10 +51,80 @@ echo "<li>\$b: "; var_dump($b); echo "</li>";
 echo "<li>\$c: "; var_dump($c); echo "</li>";
 echo "</ul>";
 
-// Liberar variables
+
+//liberar variables
 unset($a, $b, $c);
 ?>
 
+<?php
+// Parte 3: Evolución de las variables
 
+echo '<h2>Evolución de las Variables</h2>';
+echo '<ul>';
+
+// Asignaciones y evolución de las variables
+
+$a = 'PHP5';
+echo '<li>Después de asignar "PHP5" a $a, el valor de $a es: '; 
+var_dump($a); 
+echo '</li>';
+
+$z[] = &$a;
+echo '<li>Después de asignar la referencia de $a a $z[0], el valor de $z es: '; 
+var_dump($z); 
+echo '</li>';
+
+$b = 5; // Cambiar a un valor numérico para evitar problemas
+echo '<li>Después de asignar 5 a $b, el valor de $b es: '; 
+var_dump($b); 
+echo '</li>';
+
+$c = $b * 10;
+echo '<li>Después de multiplicar $b por 10, el valor de $c es: '; 
+var_dump($c); 
+echo '</li>';
+
+$a .= $b;
+echo '<li>Después de concatenar $b a $a, el valor de $a es: '; 
+var_dump($a); 
+echo '</li>';
+
+$b *= $c;
+echo '<li>Después de multiplicar $b por $c, el valor de $b es: '; 
+var_dump($b); 
+echo '</li>';
+
+$z[0] = 'MySQL';
+echo '<li>Después de asignar "MySQL" a $z[0], el valor de $z es: '; 
+var_dump($z); 
+echo '</li>';
+
+echo '</ul>';
+
+// Parte 4: Uso de $GLOBALS
+echo '<h2>Uso de $GLOBALS</h2>';
+echo '<ul>';
+
+echo '<li>$GLOBALS[\'a\']: '; 
+var_dump($GLOBALS['a']); 
+echo '</li>';
+
+echo '<li>$GLOBALS[\'b\']: '; 
+var_dump($GLOBALS['b']); 
+echo '</li>';
+
+echo '<li>$GLOBALS[\'c\']: '; 
+var_dump($GLOBALS['c']); 
+echo '</li>';
+
+echo '<li>$GLOBALS[\'z\']: '; 
+var_dump($GLOBALS['z']); 
+echo '</li>';
+
+echo '</ul>';
+
+// Liberar variables
+unset($a, $b, $c, $z);
+?>
 </body>
 </html>
